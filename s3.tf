c315e103beb00d960c530ae1480ffb1088731da9
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-05-24 15:54:28 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -134,26 +134,4 @@ resource "aws_s3_bucket" "operations" {
 
 }
 
-resource "aws_s3_bucket" "data_science" {
-  # bucket is not encrypted
-  bucket = "${local.resource_prefix.value}-data-science"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-  logging {
-    target_bucket = "${aws_s3_bucket.logs.id}"
-    target_prefix = "log/"
-  }
-  force_destroy = true
-  tags = {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "9a7c8788-5655-4708-bbc3-64ead9847f64"
-  }
-}
+
