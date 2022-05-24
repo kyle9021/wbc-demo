c7d5dd1e01fdac9599c53da67a9900acb0c8fbbf
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2022-05-24 15:35:54 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "demo-log-bucket"
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "demo-test-bucket"
+  acl    = "public"
+
+  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  }
+}
